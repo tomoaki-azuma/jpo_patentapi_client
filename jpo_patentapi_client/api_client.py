b62ef40ad6073ddce888d06589fba5305fd1391d
tomoaki-azuma <azumau@gmail.com>
2022-10-21 13:13:13 +0900

    add get_token_by_refresh_token

--- jpo_patentapi_client/api_client.py
+++ jpo_patentapi_client/api_client.py
@@ -22,3 +22,21 @@ class ApiClient:
     else:
       message = 'HTTP 400 Bad Request'
       raise api_client_error.ApiClientError(message, r.status_code, '', '')
+  
+  def get_token_by_refresh_token(self, url):
+    print('get_token_by_refresh')
+    print(self.tokens)
+    payload = {'grant_type': 'refresh_token ', 'refresh_token': self.tokens['refresh_token']}
+    r = requests.post(url, headers=self.HEADER, data=payload)
+    new_tokens  = {"access_token": "", "refresh_token": ""}
+    if (r.status_code == 200):
+      new_tokens["access_token"] = r.json()['access_token']
+      new_tokens["refresh_token"] = r.json()['refresh_token']
+      self.tokens = new_tokens
+      print(self.tokens)
+      return
+    elif (r.status_code in ['401', '429', '500', '504']):
+      raise api_client_error.ApiClientError(r.result.message, r.status_code, r.result.statusCode, r.result.remainAccessCount)
+    else:
+      message = 'HTTP 400 Bad Request'
+      raise api_client_error.ApiClientError(message, r.status_code, '', '')
--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -11,7 +11,7 @@ sys.path.append(directory)
 from jpo_patentapi_client import api_client
 from jpo_patentapi_client import api_client_error
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def user_password():
   config = configparser.ConfigParser()
   config.read(os.path.dirname(os.path.abspath(__file__)) + '/config.ini')
@@ -19,16 +19,29 @@ def user_password():
   return {'user': config['api_test']['user'], 'password': config['api_test']['password']}
 
 class TestJpoPatentapiClient:
+  @pytest.mark.skip(reason='checked')
   def test_auth(self, user_password):
     client = api_client.ApiClient(user_password['user'], user_password['password'], 'https://ip-data.jpo.go.jp/auth/token')
     
     assert (client.tokens['access_token'] != "") & (client.tokens['refresh_token'] != "")
   
+  @pytest.mark.skip(reason='checked')
   def test_unauth_illegal_username(self, user_password):
     with pytest.raises(api_client_error.ApiClientError):
       client = api_client.ApiClient('uso_user', user_password['password'], 'https://ip-data.jpo.go.jp/auth/token')
-      
+  
+  @pytest.mark.skip(reason='checked')
   def test_unauth_illegal_password(self, user_password):
     with pytest.raises(api_client_error.ApiClientError):
       client = api_client.ApiClient(user_password['user'], 'usopass', 'https://ip-data.jpo.go.jp/auth/token')
+  
+  # @pytest.mark.skip(reason='checked')
+  def test_get_token_by_refresh_token(self, user_password):
+    client = api_client.ApiClient(user_password['user'], user_password['password'], 'https://ip-data.jpo.go.jp/auth/token')
+    oldtokens = client.tokens
+    client.get_token_by_refresh_token('https://ip-data.jpo.go.jp/auth/token')
+
+    assert (client.tokens['access_token'] != "") & (client.tokens['refresh_token'] != "") & (client.tokens['access_token'] != oldtokens['access_token']) & (client.tokens['refresh_token'] != oldtokens['refresh_token'])
+
+
 
